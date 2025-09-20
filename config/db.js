77526f6e479cb9312304d2b61a07b0fcdf072ce7
neo06060 <saulcgaos@gmail.com>
2025-09-20 19:14:18 +0200

    a

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
-const mongoose = require ('mongoose');
-
 const connectDB = async (uri) => {
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected successfully');
-    } catch (err) {
-        console.error('MongoDB connection error:', err.message);
-    }
+  if (!uri) {
+    throw new Error('MongoDB URI is not defined in .env');
+  }
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1); // exit if DB connection fails
+  }
 };
 module.exports = connectDB;
