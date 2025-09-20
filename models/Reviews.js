c4b39f849bccdb738cb7d8578ca9d97a6a4a9bf6
neo06060 <saulcgaos@gmail.com>
2025-09-20 18:09:38 +0200

    a

--- models/Reviews.js
+++ models/Reviews.js
@@ -5,7 +5,6 @@ const reviewSchema = new mongoose.Schema({
     reviewer: { type: String, required: true},
     rating: { type: Number, required: true, min: 1, max: 5},
     comment:    { type: String},
-    createdAt:  { type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
