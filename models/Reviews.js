a287d96b6c8870969dc5a8697135250ec133b629
neo06060 <saulcgaos@gmail.com>
2025-09-20 18:49:45 +0200

    a

--- models/Reviews.js
+++ models/Reviews.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-    book:   { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true},
+    book:   { type: String, ref: 'Book', required: true},
     reviewer: { type: String, required: true},
     rating: { type: Number, required: true, min: 1, max: 5},
     comment:    { type: String},
